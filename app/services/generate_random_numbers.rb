c041358ea6f2fedb7daaca40947104764ef32ecd
Christian Pfeiffer <cpfeiffer325@gmail.com>
2020-07-04 17:27:01 -0600

    create random number array with unique values

---
+++ app/services/generate_random_numbers.rb
@@ -0,0 +1,10 @@
+class GenerateRandomNumbers
+  arr = []
+
+  while arr.length < 5 do
+    newNumber = rand(2..10)
+    unless arr.include?(newNumber)
+      arr.push(newNumber)
+    end
+  end
+end
