31f081b88fbea89c0065c2a2d5ddb03489d91a39
Christian Pfeiffer <cpfeiffer325@gmail.com>
2020-07-05 23:03:55 -0600

    add create and show to games controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,27 @@
 class GamesController < ApplicationController
   def new
-    
+    @game = Game.new
+  end
+
+  def create
+    @game = Game.new(game_params)
+
+    @game.save
+    redirect_to @game
+  end
+
+  def show
+    @game = Game.find(params[:id])
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(
+      :player_name,
+      :game_type,
+      :column_values,
+      :row_values,
+    )
   end
 end
