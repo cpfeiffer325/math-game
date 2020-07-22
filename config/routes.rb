7d0d2a8659ddc799d36a52ade58fb0cbf9a9d6dd
Christian Pfeiffer <cpfeiffer325@gmail.com>
2020-07-22 08:58:59 -0600

    add resources to routes for players and stats

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  resources :players, only: [:new, :create]
   resources :games, only: [:new, :create, :show]
+  resources :stats, only: [:new, :create, :show]
 
   root 'games#new'
 end
