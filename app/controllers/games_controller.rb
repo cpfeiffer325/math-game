87241a2c81547a74c67a130066e35d5b7088bbcf
Christian Pfeiffer <cpfeiffer325@gmail.com>
2020-07-22 08:58:09 -0600

    refactor games controller with a random_values method

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,12 +4,14 @@ class GamesController < ApplicationController
   end
 
   def create
-    @col = (2...10).sort_by{rand}.slice(0,5)
-    @row = (2...10).sort_by{rand}.slice(0,5)
+    @col = random_values
+    @row = random_values
 
-    @game = Game.new(game_params.merge({ 
-      column_values: @col, 
-      row_values: @row }
+    @game = Game.new(game_params.merge(
+      { 
+        column_values: @col, 
+        row_values: @row 
+      }
     ))
     @game.save
     redirect_to @game
@@ -17,13 +19,18 @@ class GamesController < ApplicationController
 
   def show
     @game = Game.find(params[:id])
+    @player = Player.current
+    # @stats = Stats.find(params[:id])
   end
 
   private
 
+  def random_values
+    (2...10).sort_by { rand }.slice(0, 5)
+  end
+
   def game_params
     params.require(:game).permit(
-      :player_name,
       :game_type,
       :column_values,
       :row_values,
