425eb1d1d568f02dbd59f55633d6a5ab7decf83e
Christian Pfeiffer <cpfeiffer325@gmail.com>
2020-07-22 08:58:26 -0600

    add create method to Players controller

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -3,5 +3,14 @@ class PlayersController < ApplicationController
   end
 
   def create
+    @player = Player.new(player_params)
+  end
+
+  private
+
+  def player_params
+    params.require(:player).permit(
+      :name
+    )
   end
 end
