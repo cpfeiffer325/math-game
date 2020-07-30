41d54465bf08f0844d28609a6b3790cfab5932cc
Christian Pfeiffer <christian.pfeiffer@zayzoon.com>
2020-07-29 21:52:02 -0600

    save game_time to events on update

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,6 +2,6 @@ class EventsController < ApplicationController
   def update
     @event = Event.find(params[:id])
     @event.game_time = Time.now - @event.created_at
-    binding.pry
+    @event.save
   end
 end
