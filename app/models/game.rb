7ac5915f900814d27d635b140701350385bdd15e
Christian Pfeiffer <cpfeiffer325@gmail.com>
2020-07-30 08:31:20 -0600

    add method to show player highscores in game

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,12 @@
 class Game < ApplicationRecord
-  
+  def self.filter_highscore_events(game:)
+    new(game: game)
+  end
+
+  def filter_highscore_events
+    @game = Game.find(self.id)
+    if Event.all.length != 0
+      Event.all.select { |e| e.game_id == @game.id && e.game_time != nil }
+    end
+  end
 end
