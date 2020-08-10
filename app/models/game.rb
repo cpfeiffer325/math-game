3be06fd7e9f52f72b9308f1d808c83357b6e01c8
Christian Pfeiffer <cpfeiffer325@gmail.com>
2020-08-09 20:13:06 -0600

    add sorting to highscores table

--- app/models/game.rb
+++ app/models/game.rb
@@ -6,7 +6,8 @@ class Game < ApplicationRecord
   def filter_highscore_events
     @game = Game.find(self.id)
     if Event.all.length != 0
-      Event.all.select { |e| e.game_id == @game.id && e.game_time != nil }
+      events = Event.all.select { |e| e.game_id == @game.id && e.game_time != nil }
+      events.sort_by { |event| event[:game_time] }
     end
   end
 end
