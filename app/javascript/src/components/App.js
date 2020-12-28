da895ea7cb8f18e0442a7209744cac13449c3183
Christian <cpfeiffer325@gmail.com>
2020-12-28 15:33:50 -0700

    Create App game layout

--- app/javascript/src/components/App.js
+++ app/javascript/src/components/App.js
@@ -1,15 +1,36 @@
 import React from 'react'
 import 'semantic-ui-css/semantic.min.css'
 
-import { Header } from 'semantic-ui-react'
+import Game from './Game'
+import HighScores from './HighScores'
+import SideMenu from './SideMenu'
+import Title from './Title'
+
+import { Grid } from 'semantic-ui-react'
 
 export default function App () {
   
   return (
     <div>
-      <Header as='h1' block textAlign='center' color='green'>
-        Mad Minute Math
-      </Header>
+      <Grid>
+        <Grid.Row columns={1}>
+          <Grid.Column width={16}>
+            <Title/>
+          </Grid.Column>
+        </Grid.Row>
+
+        <Grid.Row columns={3}>
+          <Grid.Column width={3}>
+            <SideMenu/>
+          </Grid.Column>
+          <Grid.Column width={9}>
+            <Game/>
+          </Grid.Column>
+          <Grid.Column width={4}>
+            <HighScores/>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
     </div>
   )
 }
