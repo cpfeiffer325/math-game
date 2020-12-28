49c46e6426d8404e8e799e554fdb7fee516f85c1
Christian <cpfeiffer325@gmail.com>
2020-12-28 15:03:13 -0700

    add semantic UI framework

--- app/javascript/src/components/App.js
+++ app/javascript/src/components/App.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import 'semantic-ui-css/semantic.min.css'
+
+import { Header } from 'semantic-ui-react'
 
 export default function App () {
   
   return (
-    <div>Hello World</div>
+    <div>
+      <Header as='h1' block textAlign='center' color='green'>
+        Mad Minute Math
+      </Header>
+    </div>
   )
 }
