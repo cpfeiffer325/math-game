952064db345d0be55647bb524754e6b6c88a13b1
Christian <cpfeiffer325@gmail.com>
2020-12-28 17:51:47 -0700

    Update app component layout

--- app/javascript/src/components/App.js
+++ app/javascript/src/components/App.js
@@ -1,17 +1,17 @@
 import React from 'react'
 import 'semantic-ui-css/semantic.min.css'
 
-import Game from './Game'
-import HighScores from './HighScores'
-import SideMenu from './SideMenu'
+import GameContainer from './GameContainer'
+import Highscores from './Highscores'
+import GameOptions from './GameOptions'
 import Title from './Title'
 
-import { Grid } from 'semantic-ui-react'
+import { Grid, Segment } from 'semantic-ui-react'
 
 export default function App () {
   
   return (
-    <div>
+    <div style={{margin: "20px"}}>
       <Grid>
         <Grid.Row columns={1}>
           <Grid.Column width={16}>
@@ -20,14 +20,12 @@ export default function App () {
         </Grid.Row>
 
         <Grid.Row columns={3}>
-          <Grid.Column width={3}>
-            <SideMenu/>
+          <Grid.Column width={10}>
+            <Segment><GameContainer /></Segment>
           </Grid.Column>
-          <Grid.Column width={9}>
-            <Game/>
-          </Grid.Column>
-          <Grid.Column width={4}>
-            <HighScores/>
+          <Grid.Column width={6}>
+            <Segment><GameOptions /></Segment>
+            <Segment><Highscores /></Segment>
           </Grid.Column>
         </Grid.Row>
       </Grid>
