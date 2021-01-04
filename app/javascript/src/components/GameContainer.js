7156a163f67daaa4f75c0811d19456fdf01e4823
Christian <cpfeiffer325@gmail.com>
2021-01-03 22:52:35 -0700

    refactor game container

--- app/javascript/src/components/GameContainer.js
+++ app/javascript/src/components/GameContainer.js
@@ -2,15 +2,17 @@ import React from 'react'
 import { Tab } from 'semantic-ui-react'
 import Game from './Game'
 
-
-const panes = [
-  { menuItem: 'Addition', render: () => <Tab.Pane><Game/></Tab.Pane> },
-  { menuItem: 'Subtraction', render: () => <Tab.Pane><Game/></Tab.Pane> },
-  { menuItem: 'Multiplication', render: () => <Tab.Pane><Game/></Tab.Pane> },
-  { menuItem: 'Division', render: () => <Tab.Pane><Game/></Tab.Pane> },
-]
-
 export default function GameContainer () {
+
+  const game = <Game/>
+  
+  const panes = [
+    { menuItem: 'Addition', render: () => <Tab.Pane>{game}</Tab.Pane> },
+    { menuItem: 'Subtraction', render: () => <Tab.Pane>{game}</Tab.Pane> },
+    { menuItem: 'Multiplication', render: () => <Tab.Pane>{game}</Tab.Pane> },
+    { menuItem: 'Division', render: () => <Tab.Pane>{game}</Tab.Pane> },
+  ]
+
   return (
     <Tab panes={panes} />
   )
